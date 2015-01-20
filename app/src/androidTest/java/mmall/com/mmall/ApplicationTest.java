287bf0c64d32055781a874bcebf1fae32e5d301d
Nicol <nicol.xiang@missionsky.com>
2015-01-20 22:35:01 +0800

    Initiate

---
+++ app/src/androidTest/java/mmall/com/mmall/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mmall.com.mmall;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
