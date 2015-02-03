07bcfc4167d5740bdf26aa0527688326a6429b43
NicolXiangXiang <xyening@gmail.com>
2015-02-03 17:41:17 +0800

    at MS

--- app/src/main/java/mmall/com/mmall/HomePageFragment.java
+++ app/src/main/java/mmall/com/mmall/HomePageFragment.java
@@ -10,7 +10,12 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.PopupMenu;
+
+import java.util.ArrayList;
+
+import mmall.com.mmall.model.HomePageDataModel;
 
 
 /**
@@ -23,6 +28,7 @@ import android.widget.PopupMenu;
 public class HomePageFragment extends Fragment {
     private Button mSearchButton;
     private Button mSettingButton;
+    private ListView mListView;
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -50,6 +56,7 @@ public class HomePageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.homepage, container, false);
+        mListView=(ListView)v.findViewById(R.id.lv_homepage);
         mSearchButton=(Button)v.findViewById(R.id.btn_search);
         mSettingButton=(Button)v.findViewById(R.id.btn_setting);
         mSettingButton.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +70,8 @@ public class HomePageFragment extends Fragment {
                 popupMenu.show();
             }
         });
+        HomePageListAdapter adapter=new HomePageListAdapter(new ArrayList<HomePageDataModel>(),HomePageFragment.this.getActivity());
+        mListView.setAdapter(adapter);
         return v;
     }
 }
---
+++ app/src/main/java/mmall/com/mmall/model/HomePageDataModel.java
@@ -0,0 +1,13 @@
+package mmall.com.mmall.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nicol.xiang on 2015/2/3.
+ */
+public class HomePageDataModel {
+    public List<ActivitiesModel> Activies=new ArrayList<ActivitiesModel>();
+    public List<ProductModel> Recommends=new ArrayList<ProductModel>();
+}
